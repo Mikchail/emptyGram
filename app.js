a392dee361909c62cb70dafaa2b39c9e294f0f5c
Michal R <refs1@yandex.ru>
2020-03-01 03:11:47 +0300

    test3

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ app.use(roters);
 //       res.redirect('/')
 // })
 if (process.env.NODE_ENV === 'production') {
-  // app.use('/', express.static(path.join(__dirname, 'dist')))
+  app.use('/', express.static(path.join(__dirname, 'client/dist')))
 
 
   app.get('*', (req, res) => {
